03742ca098453e0e1ff7dd43b00b3df9dff1331c
vorakl <me@vorakl.name>
2015-07-22 18:48:39 +0200

    Has been written a couple tests

--- prun-test.sh
+++ prun-test.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env roundup
 
-describe "Unit tests for prun"
+describe "Tests for prun"
 
-it_is_pass() {
-   return 0
+it_is_usage() {
+   output=$(./prun help | grep Usage)
+   test "${output}" = 'Usage: prun "dest1 ...|@file|- ..." "command args"'
 }
 
+it_is_destination_parser() {
+   output=$(set +e; echo "" | DEBUG=1 ./prun "-" ":" 2>&1; :)
+   test "${output}" = "DEBUG: Destination not found"
+}
