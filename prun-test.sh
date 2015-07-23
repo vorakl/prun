b5b2ca701e054a6e48d94b932a7ee92c907c8523
vorakl <me@vorakl.name>
2015-07-23 13:14:24 +0200

    configuring travis

--- prun-test.sh
+++ prun-test.sh
@@ -3,11 +3,11 @@
 describe "Tests for prun"
 
 it_is_usage() {
-   output=$(set +e; ./prun help | grep Usage; :; )
+   output=$(set +e; ./prun help | grep Usage; 2>&1 : )
    test "${output}" = 'Usage: prun "dest1 ...|@file|- ..." "command args"'
 }
 
 it_is_destination_parser() {
-   output=$(set +e; echo "" | DEBUG=1 ./prun "-" ":" 2>&1; :; )
+   output=$(set +e; echo "" | DEBUG=1 ./prun "-" ":" 2>&1; : )
    test "${output}" = "DEBUG: Destination not found"
 }
